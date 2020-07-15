f0cb4e5acf8982b92b0675052e06e9dc33556965
=Jhosua Pachon <jhosuapachon@gmail.com>
2020-07-15 12:01:01 -0500

    fix(list): added two divs title and desc

--- src/components/ListContributor/ListContributor.jsx
+++ src/components/ListContributor/ListContributor.jsx
@@ -5,6 +5,14 @@ import './ListContributor.scss';
 const ListContributor = () => {
   return (
     <div className="listContainer">
+      <div className="listContainer__title">
+        <h2>Contribuidores</h2>
+      </div>
+      <div className="listContainer__desc">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod culpa aut veritatis dolor tempora rem autem earum, non ducimus quae totam perspiciatis tempore voluptatibus fugit aliquam veniam minima dolorum vero.
+        Quidem voluptate corporis aliquam iusto, magni hic repudiandae deserunt nam ratione? Mollitia, error labore, nemo explicabo consequatur neque, maiores molestias optio architecto nulla harum laboriosam assumenda exercitationem facere voluptates quam!
+       </p>
+      </div>
       <CardContributor />
       <CardContributor />
       <CardContributor />
