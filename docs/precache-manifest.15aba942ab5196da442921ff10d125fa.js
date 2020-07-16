7b531101fb053464a80ecd7c3153610fdb402db8
Pablo <pabloosabaterr@gmail.com>
2020-07-16 02:43:01 +0200

    feat/live

---
+++ docs/precache-manifest.15aba942ab5196da442921ff10d125fa.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e85a5fedd004405fa7d9a1731dc005ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b490fed81473fb57a718",
+    "url": "/static/css/main.d79920e0.chunk.css"
+  },
+  {
+    "revision": "04615a28d7b3f0bb3750",
+    "url": "/static/js/2.4ac14520.chunk.js"
+  },
+  {
+    "revision": "0c7f7415c0ec92798fd914561f3edc54",
+    "url": "/static/js/2.4ac14520.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b490fed81473fb57a718",
+    "url": "/static/js/main.8f36e450.chunk.js"
+  },
+  {
+    "revision": "64bffb3210c7038b56a7",
+    "url": "/static/js/runtime-main.a838e22f.js"
+  },
+  {
+    "revision": "00c464f2eb8991d0bcf25a29dcd071f3",
+    "url": "/static/media/FaztLogo.00c464f2.svg"
+  },
+  {
+    "revision": "1025a6e0fb0fa86f17f57cc82a6b9756",
+    "url": "/static/media/OpenSans-Bold.1025a6e0.ttf"
+  },
+  {
+    "revision": "2d0bdc8df10dee036ca3bedf6f3647c6",
+    "url": "/static/media/OpenSans-Light.2d0bdc8d.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "ba5cde21eeea0d57ab7efefc99596cce",
+    "url": "/static/media/OpenSans-SemiBold.ba5cde21.ttf"
+  },
+  {
+    "revision": "5ebb24ee1112dd9562629375c387879a",
+    "url": "/static/media/Roboto-Black.5ebb24ee.ttf"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/static/media/Roboto-Bold.e07df86c.ttf"
+  },
+  {
+    "revision": "88823c2015ffd5fa89d567e17297a137",
+    "url": "/static/media/Roboto-Light.88823c20.ttf"
+  },
+  {
+    "revision": "58aef543c97bbaf6a9896e8484456d98",
+    "url": "/static/media/Roboto-Medium.58aef543.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "321de678e592d0b8f44f1a82d7ca4b62",
+    "url": "/static/media/Roboto-Thin.321de678.ttf"
+  },
+  {
+    "revision": "66634a4cabfd0da8e6da97e67feac895",
+    "url": "/static/media/arrow.66634a4c.svg"
+  },
+  {
+    "revision": "c1d813fa063073432c26dc9b1249fd31",
+    "url": "/static/media/discord-brands.c1d813fa.svg"
+  },
+  {
+    "revision": "ba87dfa2ac0da9cc1594dea5f9b8fa0f",
+    "url": "/static/media/faztCommunity.ba87dfa2.png"
+  },
+  {
+    "revision": "a8cd76c1934dc020e510f93a33774305",
+    "url": "/static/media/heart.a8cd76c1.svg"
+  },
+  {
+    "revision": "02cc841c75b7551b44d146112575d45d",
+    "url": "/static/media/parallax.02cc841c.svg"
+  }
+]);
