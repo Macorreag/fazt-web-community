aee350f60953af803878410fcae756bbb60e6d1c
=Jhosua Pachon <jhosuapachon@gmail.com>
2020-07-15 21:15:12 -0500

    fix(listcard): added copyright

--- src/components/ListContributor/ListContributor.jsx
+++ src/components/ListContributor/ListContributor.jsx
@@ -1,3 +1,5 @@
+/* Copyright 2020 Fazt Community ~ All rights reserved. MIT license. */
+
 import React from 'react';
 import CardContributor from '../CardContributor/CardContributor';
 import './ListContributor.scss';
@@ -9,9 +11,13 @@ const ListContributor = () => {
         <h2>Contribuidores</h2>
       </div>
       <div className="listContainer__desc">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod culpa aut veritatis dolor tempora rem autem earum, non ducimus quae totam perspiciatis tempore voluptatibus fugit aliquam veniam minima dolorum vero.
-        Quidem voluptate corporis aliquam iusto, magni hic repudiandae deserunt nam ratione? Mollitia, error labore, nemo explicabo consequatur neque, maiores molestias optio architecto nulla harum laboriosam assumenda exercitationem facere voluptates quam!
-       </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod culpa aut veritatis dolor tempora rem
+          autem earum, non ducimus quae totam perspiciatis tempore voluptatibus fugit aliquam veniam minima
+          dolorum vero. Quidem voluptate corporis aliquam iusto, magni hic repudiandae deserunt nam ratione?
+          Mollitia, error labore, nemo explicabo consequatur neque, maiores molestias optio architecto nulla
+          harum laboriosam assumenda exercitationem facere voluptates quam!
+        </p>
       </div>
       <CardContributor />
       <CardContributor />
